688bf0822cfb8f03bc39207e1e00a2d59873b17e
kundalrahul <kundalrahul11@gmail.com>
2024-08-31 02:29:30 -0400

    Defend code

---
+++ my_project/my_app/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class User(models.Model):
+    username = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.username
